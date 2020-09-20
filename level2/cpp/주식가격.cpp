78eb2d9cb93c04aea6376b303ba3dd28d126ed13
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-09-20 21:19:38 +0900

    solving level2

---
+++ level2/cpp/주식가격.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    for(int i=0; i<prices.size()-1; ++i){
+        int cnt = 0;
+        for(int j=i+1; j<prices.size(); ++j){
+            if(prices[i]>prices[j]){
+               cnt += 1;
+                break;
+            }
+            else{
+                cnt += 1;
+            }
+        }
+        answer.push_back(cnt);
+    }
+    answer.push_back(0);
+    return answer;
+}
